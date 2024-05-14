b2aed3255540772782c0a3bfa7263d5b29ae5c7c
Steven James, Ph.D <steven.p.james@gmail.com>
2024-05-14 12:04:46 -1000

    Delete config.php
    Security Removal

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config.php
-define('DB_HOST', 'mysql.space-buddha.com');
-define('DB_USERNAME', 'steven_james');
-define('DB_PASSWORD', 'xNbz2zUY5rWW6Vy');
-define('DB_NAME', 'trek_episodes');
-?>
